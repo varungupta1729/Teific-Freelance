6b6eadd505dcb7b54e13be145fab447f63679c60
Varun Gupta <varungupta1228@gmail.com>
2024-11-25 23:50:59 +0530

    added linear transition

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,69 @@
-import React, { useState } from 'react'
-import logo from "../Assets/images/Logo.png"
-import { HeaderNav } from '../static/data'
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+import logo from "../Assets/images/Logo.png";
+import { HeaderNav } from "../static/data";
+import { Link } from "react-router-dom";
 import { BsList } from "react-icons/bs";
 import { RxCross1 } from "react-icons/rx";
-const Header = ({position}) => {
-  const[active , setActive] = useState(false);
+import {motion} from 'motion/react'
+import { slideAnimation } from "../motion/motion";
+const Header = ({ position }) => {
+  const [active, setActive] = useState(false);
   return (
-    <div className={` ${ position?"my-7":"absolute top-7 "} z-10 w-full flex justify-between lg:items-center lg:px-36 px-7 `}>
-     <Link to="/"> <img src={logo} alt='logo' className='w-10 lg:w-full ' /></Link>
-      <div className={`lg:flex bg-gradient-to-t from-[#FFFFFF24] to-[#FFFFFF00] ${active ?"flex":"hidden"} px-10 py-4 rounded-2xl`}> 
-  
-       <ul className={`flex flex-col lg:flex-row gap-10 `}>
-        {
-            HeaderNav.map((item)=> <Link to={item.link}><li>{item.content}</li></Link>)
-        }
-       </ul>
+    <div
+      className={` ${
+        position ? "my-7" : "absolute top-7 "
+      } z-10 w-full flex justify-between lg:items-center lg:px-36 px-7 `}
+    >
+      <Link to="/">
+        {" "}
+        <img src={logo} alt="logo" className="w-10 lg:w-full " />
+      </Link>
+      <div
+        className={`lg:flex bg-gradient-to-t from-[#FFFFFF24] to-[#FFFFFF00] ${
+          active
+            ? "flex justify-center flex-col items-center text-center bg-black"
+            : "hidden"
+        } lg:px-10 py-4 rounded-2xl absolute top-0 left-0 w-full lg:w-auto lg:static `}
+      >
+        {active && (
+          <div className="w-full flex justify-end px-10 ">
+            <RxCross1
+              className=""
+              size={30}
+              onClick={() => setActive(!active)}
+            />
+          </div>
+        )}
+        <ul className={`flex flex-col lg:flex-row gap-10 `}>
+          {HeaderNav.map((item) => (
+            <Link to={item.link}>
+              <motion.li initial="initial"
+  whileInView="animate"
+  exit="exit"
+  viewport={{ once: false, amount: 0.7 }} // Ensures it animates only once when 50% is in view
+  variants={slideAnimation('up')}>{item.content}</motion.li>
+            </Link>
+          ))}
+        </ul>
       </div>
-      <div className=' hidden lg:block bg-gradient-to-t from-[#FFFFFF24] to-[#FFFFFF00] px-10 py-4 rounded-2xl'> 
-        <span className=''>
-            Explore Now
-        </span>
-       
+      <div className=" hidden lg:block bg-gradient-to-t from-[#FFFFFF24] to-[#FFFFFF00] px-10 py-4 rounded-2xl">
+        <span className="">Explore Now</span>
       </div>
-      {
-          active ? (<RxCross1 className='p-0' size={30}   onClick={()=>setActive(!active)}/>):(<BsList size={40}  className='lg:hidden' onClick={()=>setActive(!active)}/>)
-        }
+      {active ? (
+        <RxCross1
+          className="p-0"
+          size={30}
+          onClick={() => setActive(!active)}
+        />
+      ) : (
+        <BsList
+          size={40}
+          className="lg:hidden"
+          onClick={() => setActive(!active)}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
